102d2a840b1ac43df80a8e2e884ddbbfab526044
Nikhil4474 <ninuganti15@gmail.com>
2022-10-28 18:12:06 -0500

    first commit

---
+++ screens/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Alert, TextInput } from 'react-native';
+import firebase from 'firebase';
+
+export default class Login extends React.Component {
+  render() {
+    return <Text>Login Screen</Text>;
+  }
+}
