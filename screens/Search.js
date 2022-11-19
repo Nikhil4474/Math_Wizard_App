2af80de7f5735605976d87fc40e7634fd5e575f6
Nikhil4474 <ninuganti15@gmail.com>
2022-11-18 19:08:13 -0600

    QR code scanner added

--- screens/Search.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Alert, TextInput } from 'react-native';
-
-export default class Search extends React.Component {
-  render() {
-    return <Text>This is the Search Screen</Text>;
-  }
-}
